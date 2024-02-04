f96c05518ec40310e26679f96316e261737450d7
k-ruko <heksagonka@gmail.com>
2024-02-04 21:13:19 +0800

    code2

---
+++ calculator2.cpp
@@ -0,0 +1,45 @@
+//calculator 
+#include <iostream>
+using namespace std;
+int main()
+{
+	int num1, num2, num3;
+	char op;
+	
+	
+		cout << "  " << endl;
+		cin >> num1;
+		
+		cout << "   " << endl; 
+		cin >> num2;
+		
+		cout << "  " << endl;
+		cin >> num3;
+	
+	
+	cout << "chosse operator : " << endl;
+	
+	switch(op)
+	{ 
+		case('+'):
+		cout << num1+num2+num3 << endl;
+	break;
+	
+		case('-'):
+			cout << num1-num2-num3 << endl;
+	break;
+	
+		case('/'):
+			cout << num1/num2/num3 << endl;
+	break;
+		case('*'):
+			cout << num1*num2*num3 << endl; 
+	break;
+	
+	default:
+		cout<<"tidak ada operator seperti itu "<< endl;
+		 
+	} 
+	
+	return 0;
+}
