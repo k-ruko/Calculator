ac222462979e963ce4d682cd94d082b08d30153c
k-ruko <heksagonka@gmail.com>
2024-02-04 21:51:39 +0800

    nothing

---
+++ coba.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+int angka;
+
+cout << "masukkan angka: "<< endl  ;
+cin >> angka;
+
+if(angka >= 50){
+
+	cout << "lulus " << endl;
+}
+	else {
+		angka < 50 ;
+		cout << "tidak lulus" << endl;
+	}  
+	
+	return 0;
+}
+
